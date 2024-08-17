3f3d9a87770084672709dc1a9af78bdcbe9806ac
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-18 01:34:07 +0600

    working on Navbar

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <div>
+                <div >
+                    <nav className="navbar navbar-expand-lg ">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#">
+                                Skill <span>Shoot</span>
+                            </a>
+
+
+
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+
+
+                            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                                <div>
+                                    <div>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                        <Link href="/">
+                                            Home
+                                        </Link>
+                                    </div>
+                                </div>
+                                {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                    </li>
+                                </ul>
+                                <form className="d-flex" role="search">
+                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                        <button className="btn btn-outline-success" type="submit">Search</button>
+                                </form> */}
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
