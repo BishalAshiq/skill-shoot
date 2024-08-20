0854113b5c34a40c1d9c790932eef6861be2d070
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-08-20 15:31:13 +0600

    working on homepage

--- src/components/homeAll/PopCourseWeek/PopCourseWeek.jsx
+++ src/components/homeAll/PopCourseWeek/PopCourseWeek.jsx
@@ -1,47 +1,19 @@
-"use client"
-import React from 'react';
-import style from "./PopCourseWeek.module.css";
-import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+"use client"; // Required for client-side components in Next.js 13+
 
+import React from "react";
+
+
+import style from "./PopCourseWeek.module.css";
 
 
 
 const PopCourseWeek = () => {
-    var settings = {
+    const settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        slidesToShow: 1,
+        slidesToScroll: 1,
     };
     return (
         <div>
@@ -55,21 +27,30 @@ const PopCourseWeek = () => {
 
 
                 <div>
+
+                    <div>
+                        I want a slider in next js pattern
+
+                    </div>
+
+{/* 
                     <Slider {...settings}>
                         <div>
-                            <h3>Hello</h3>
+                            <h3>Slide 1</h3>
                         </div>
                         <div>
-                            <h3>Dullo</h3>
+                            <h3>Slide 2</h3>
                         </div>
                         <div>
-                            <h3>Dullo</h3>
+                            <h3>Slide 3</h3>
                         </div>
                         <div>
-                            <h3>Dullo</h3>
+                            <h3>Slide 4</h3>
                         </div>
-                        
-                    </Slider>
+                        <div>
+                            <h3>Slide 5</h3>
+                        </div>
+                    </Slider> */}
                 </div>
 
             </div>
